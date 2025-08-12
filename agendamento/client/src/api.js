0012cfd2591d96dc64839296183949b2ffe74065
nskilleer <nathandasilva052@gmail.com>
2025-08-12 18:42:23 -0300

    up

---
+++ agendamento/client/src/api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+// URL base da sua API. Altere para a URL do seu servidor Render quando implantar.
+const api = axios.create({
+    baseURL: 'http://localhost:4000/api',
+});
+
+export const setAuthToken = (token) => {
+    if (token) {
+        api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    } else {
+        delete api.defaults.headers.common['Authorization'];
+    }
+};
+
+export default api;
